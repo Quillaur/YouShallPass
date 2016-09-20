5535bd05fcb8d40608a4a3b0e63967cc967226a7
Quillaur <quillaur@outlook.com>
2016-09-20 18:09:51 +0300

    Code was simplified by using new features from my OverToolkit.

--- You Shall Pass/Views/AboutView.xaml.cs
+++ You Shall Pass/Views/AboutView.xaml.cs
@@ -1,28 +1,12 @@
-﻿using OverToolkit.Enums;
-using OverToolkit.Helpers;
-using System;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using OverToolkit.Controls;
 
 namespace You_Shall_Pass.Views
 {
-    public sealed partial class AboutView : Page
+    public sealed partial class AboutView : View
     {
         public AboutView()
         {
             InitializeComponent();
-        }
-
-        private async void Page_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (DeviceTypeHelper.GetDeviceFormFactorType() != DeviceFormFactorType.Phone)
-                return;
-
-            if (ApplicationView.GetForCurrentView().Orientation == ApplicationViewOrientation.Portrait)
-                await StatusBar.GetForCurrentView().ShowAsync();
-            else
-                await StatusBar.GetForCurrentView().HideAsync();
         }
     }
 }
